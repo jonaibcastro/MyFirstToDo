7ea83cc060a26959fac34ac233503b175610262e
Jonaib Castro <jonaibcastro2025@gmail.com>
2026-05-07 16:58:57 -0300

    retirando comentário sobre senha db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2'
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',// se tiver senha no seu MySQL, coloca aqui
+    password: ''
     database: 'myfirsttodo'
 })
 
